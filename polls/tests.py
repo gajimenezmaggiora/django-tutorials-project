8323592009f5b99b79046365c82e1d037c5cdaa1
Gustavo Jimenez-Maggiora <gajimenezmaggiora@gmail.com>
2013-05-18 05:03:54 -0700

    Fixed bug in models.py and added unit tests to tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -4,13 +4,30 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
+
+from django.utils import timezone
 
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from polls.models import Poll
+
+class PollMethodTests(TestCase):
+    
+    def test_was_published_recently_with_old_poll(self):
         """
-        Tests that 1 + 1 always equals 2.
+        was_published_recently() should return False for polls whose pub_date
+        is older than 1 day
         """
-        self.assertEqual(1 + 1, 2)
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+
+    
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
